c5def19cbbcfdf0fbeec1a0e2cda7f16b1b5818c
mohfirdaus22 <thepashter6@gmail.com>
2023-12-28 20:21:05 +0700

    Add val coroutineScope

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -1,7 +1,9 @@
 package com.example.consumerestapi.ui.home.screen
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumerestapi.ui.PenyediaViewModel
@@ -14,5 +16,6 @@ fun EntryKontakScreen(
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
