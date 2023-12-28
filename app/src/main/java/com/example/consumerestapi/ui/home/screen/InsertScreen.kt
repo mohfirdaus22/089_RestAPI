153dbde63bd32ee5210c1bcece2a68f3863be10f
mohfirdaus22 <thepashter6@gmail.com>
2023-12-28 20:20:33 +0700

    Add InsertScreen.kt

---
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -0,0 +1,18 @@
+package com.example.consumerestapi.ui.home.screen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumerestapi.ui.PenyediaViewModel
+import com.example.pakeapi.ui.home.viewmodel.InsertViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKontakScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
