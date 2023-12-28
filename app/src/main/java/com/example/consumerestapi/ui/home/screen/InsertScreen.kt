0f8a81d94f7ab8967a4fc7305f8becd318fdb137
mohfirdaus22 <thepashter6@gmail.com>
2023-12-28 20:24:54 +0700

    Add Scaffold

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -1,10 +1,13 @@
 package com.example.consumerestapi.ui.home.screen
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumerestapi.ui.PenyediaViewModel
 import com.example.pakeapi.ui.home.viewmodel.InsertViewModel
@@ -18,4 +21,20 @@ fun EntryKontakScreen(
 ){
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(){
+            innerPadding ->
+        EntryKontakBody(
+            insertUiState = viewModel.insertkontakState,
+            onSiswaValueChange = viewModel::updateInsertKontakState,
+            onSaveClick = { coroutineScope.launch {
+                viewModel.insertKontak()
+                navigateBack()
+            }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
